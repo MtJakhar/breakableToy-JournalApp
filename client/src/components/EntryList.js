9d8689f17c6846bbdc020a6c9e6eb7922ab8a663
MtJakhar <thvsd.10fingers@gmail.com>
2023-02-22 13:48:05 -0500

    made entries postable to list

--- client/src/components/EntryList.js
+++ client/src/components/EntryList.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import EntryListItem from './EntryListItem'
 
-const EntryList = () => {
+const EntryList = (props) => {
+
+  const [entryList, setEntryList] = useState([])
+
+  const getEntries = async () => {
+    try {
+      const response = await fetch("/api/v1/entry")
+      if (!response.ok) {
+        throw new Error(`${response.status} (${response.statusText})`)
+      } else {
+        const body = await response.json()
+        setEntryList(body.entries)
+      }
+    } catch(error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
+ 
+  useEffect(() => {
+    getEntries()
+  }, [])
+
+  const entriesItems = entryList.map(entry => {
+    return (
+      <EntryListItem
+        key={entry.id}
+        entry={entry}
+      />
+    )
+  })
+
   return (
-    <div>EntryList</div>
+    <div>EntryList
+      {entriesItems}
+    </div>
+    
   )
 }
 
